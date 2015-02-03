9668582c8a5db36365ac0f0f127d97fa2273c1ab
Jay Wiggins <jay@irk.com>
2015-02-03 07:04:35 -0700

    working on getting packaging working

--- test/canvas-pie.test.js
+++ test/canvas-pie.test.js
@@ -9,8 +9,12 @@ describe("canvas pie shape", function() {
   });
   
   it("can convert degrees to radians", function() {
-    var exp = Math.round(10000 * Pie.d2r(90)) / 10000;
-    expect(exp).toEqual(1.5708, 'conversion was wrong, should be 1.57079633');
+    // clever floating point rounding technique via found:
+    // http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-in-javascript
+    var exp = +Pie.d2r(57.2957795).toFixed(5); // 57.2957795 degrees ~ 1 rad
+    expect(exp).toEqual(1, 'conversion was wrong, should be 1');
+    var exp = +Pie.d2r(1).toFixed(6);
+    expect(exp).toEqual(0.017453, 'conversion was wrong, should be 0.017453');
   });
   
 });
